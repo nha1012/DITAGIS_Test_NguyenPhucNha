61d912bd1a24f0e42f444bec8c630a98334cfec2
Indra Hodu(Nha) <tech11.qtdata@gmail.com>
2020-10-06 11:22:01 +0700

    setup Pagination

--- www/src/component/ProductList.js
+++ www/src/component/ProductList.js
@@ -3,14 +3,30 @@ import Product from "../component/Product";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProduct } from "../redux/action/product";
 import Loader from "react-loader-spinner";
+import Pagination from "@material-ui/lab/Pagination";
+
 const ProductList = () => {
   const isLoading = useSelector((state) => state.productReducer.isLoading);
-  const allProduct = useSelector((state) => state.productReducer.allProduct);
+  let allProduct = useSelector((state) => state.productReducer.allProduct);
   const dispatch = useDispatch();
-  let newAllProduct = [];
+  const pageLimit = 10
+  const [currentPage, setCurentPage] = useState(1)
+  const firstItem = (currentPage - 1 ) * 10
+  const lastItem = pageLimit * currentPage
+  let totalPage = undefined
+
+  if (Array.isArray(allProduct)) {
+    totalPage = allProduct.length / 10
+    allProduct = allProduct.slice(firstItem, lastItem)
+  }
+
   useEffect(() => {
     if (!allProduct) dispatch(fetchProduct());
   }, []);
+
+  const handleChangePage = (event, value) => {
+    setCurentPage(value);
+  };
 
   if (!isLoading && allProduct)
     return (
@@ -20,6 +36,7 @@ const ProductList = () => {
             return <Product key={index} details={value}></Product>;
           })}
         </div>
+        <Pagination count={totalPage} page={currentPage} onChange={handleChangePage} size="large" />
       </div>
     );
   else {
